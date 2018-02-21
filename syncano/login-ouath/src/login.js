433a5c06132ce2f00e52043a22502f5fe28dd204
grzegorz <grzegorz@polarbits.co>
2018-02-21 22:20:23 +0100

    fix: ouath to develop

--- syncano/login-ouath/src/login.js
+++ syncano/login-ouath/src/login.js
@@ -1,37 +1,46 @@
-import Syncano from '@syncano/core';
-import dotenv from 'dotenv';
-import axios from 'axios';
+import Syncano from "@syncano/core";
+import dotenv from "dotenv";
+import axios from "axios";
 
 dotenv.config();
 
-export default (ctx) => {
-  const {response, users} = new Syncano(ctx)
-  // const CLIENT_ID=process.env.CLIENT_ID;
-  // const CLIENT_SECRET = process.env.CLIENT_SECRET;
-  const CLIENT_ID='666f7072bbaba9fc4e75'
-  const CLIENT_SECRET = ctx.config.GITHUB_CLIENT_SECRET
+export default ctx => {
+  const { response, users } = new Syncano(ctx);
+  const CLIENT_ID = "666f7072bbaba9fc4e75";
+  const CLIENT_SECRET = ctx.config.GITHUB_CLIENT_SECRET;
   if (ctx.args.code) {
-
-    let access_token;
-    console.log(CLIENT_SECRET);
-    const result = axios.post('https://github.com/login/oauth/access_token',
-                            {client_id: CLIENT_ID,
-                             client_secret: CLIENT_SECRET,
-                             code: ctx.args.code})
-                             .then( (rez) => {
-                              //  console.log(JSON.parse(rez.data)['access_token'])
-                              access_token = rez.data;
-                              console.log(rez.data);
-                              response.json({
-                                message: `Hello ${ctx.args.code}! Here is your token:` + access_token
-                              })
-                             });
-  
-    // access_token = JSON.parse(result)['access_token']
-
+    const result = axios
+      .post("https://github.com/login/oauth/access_token", {
+        client_id: CLIENT_ID,
+        client_secret: CLIENT_SECRET,
+        code: ctx.args.code
+      })
+      .then(rez => {
+        // console.log(rez)
+        var str = rez.data;
+        var matches = str.match(/access_token=([^&]*)/);
+        const access_token = matches[1];
+        axios
+          .get("https://api.github.com/user", {
+            params: { access_token: access_token }
+          })
+          .then(ress => {
+            var user = ress.data;
+            // users
+            console.log(user)
+            response.json({
+              message:
+                `Hello ${ctx.args.code}! id: ${user.id}. Here is your token:` +
+                access_token
+            });
+          });
+      });
   } else {
-    response.json({
-      message: 'You have to be authorized with code!'
-    }, 400)
+    response.json(
+      {
+        message: "You have to be authorized with code!"
+      },
+      400
+    );
   }
-}
+};
