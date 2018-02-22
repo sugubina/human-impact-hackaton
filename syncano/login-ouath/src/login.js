6a5a61369b8a25341529dab62115a7990eab5fe3
grzegorz <grzegorz@polarbits.co>
2018-02-22 01:41:50 +0100

    feat: add custom user class

--- syncano/login-ouath/src/login.js
+++ syncano/login-ouath/src/login.js
@@ -5,7 +5,7 @@ import axios from "axios";
 dotenv.config();
 
 export default ctx => {
-  const { response, users } = new Syncano(ctx);
+  const { response, users, data } = new Syncano(ctx);
   const CLIENT_ID = "666f7072bbaba9fc4e75";
   const CLIENT_SECRET = ctx.config.GITHUB_CLIENT_SECRET;
   if (ctx.args.code) {
@@ -25,14 +25,24 @@ export default ctx => {
             params: { access_token: access_token }
           })
           .then(ress => {
-            var user = ress.data;
+            const user = ress.data;
+
+            // users.where({username:user.id})
             // users
-            console.log(user)
-            response.json({
-              message:
-                `Hello ${ctx.args.code}! id: ${user.id}. Here is your token:` +
-                access_token
-            });
+            console.log('user operation');
+
+            // users.where({username: user.id}).list().then(() =>{
+            //   console.log('test');
+            // });
+              data.participants.create(
+                { gitId: user.id ,accessToken: access_token, gitUrl: user.url, avatarUrl: user.avatar_url }
+              )
+              .then(() => {
+                response.json({
+                  message:
+                    `Hello ${ctx.args.code}! id: ${user.id}. Here is your token:` + access_token
+                });
+              });
           });
       });
   } else {
